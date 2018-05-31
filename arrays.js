4fd1699a521aee4a8b137d8720c1b0109f54d10c
Kyle Neale <kyle.a.neale@gmail.com>
2018-05-31 14:48:42 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,11 +1,13 @@
 chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
 
 function addElementToBeginningOfArray(array, element) {
-  var idk = new Array();
-  return idk.unshift(element);
+  var idk = new Array(...array);
+  idk.unshift(element);
+  return idk;
 }
 
 function destructivelyAddElementToBeginningOfArray(array, element) {
-  
+  array.unshift(element);
+  return array;
 }
 
